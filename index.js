10d9e637ff9eb9ac4a6e7195012bf1b9ed572bb8
Nico Fannelli <44193905+NicoFannelli@users.noreply.github.com>
2019-10-20 22:03:05 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
 module.exports = function showRNG(mod) {
-    mod.hook('S_REGISTER_EVOLUTION_ITEM', 2, e => {
+    mod.hook('S_REGISTER_EVOLUTION_ITEM', 3, e => {
         e.hideSuccessChance = false;
         return true;
     });
 
-    mod.hook('S_REGISTER_ENCHANT_ITEM', 2, e => {
+    mod.hook('S_REGISTER_ENCHANT_ITEM', 3, e => {
         e.hideSuccessChance = false;
         return true;
     });
